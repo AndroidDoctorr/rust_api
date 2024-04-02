16f6390203f2423edf6850428f08cfaea3c77e53
AndroidDoctorr <gbartthenerd@gmail.com>
2024-04-01 23:16:16 -0400

    added sqlx to Cargo.toml and connection string to main to set up a SQL connection

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,18 @@ mod handlers;
 mod models;
 
 #[tokio::main]
-async fn main() {
-    let routes = routes::routes();
+async fn main() -> Result<(), sqlx::Error> {
+    let connection_string = "Server=localhost,1433;Database=rust_blog;User Id=sa;Password=P45Sw0rd!;Encrypt=false;TrustServerCertificate=true;";
+    let pool = sqlx::mssql::MssqlPoolOptions::new()
+        .connect(connection_string)
+        .await?;
 
-    println!("Server started at http://localhost:8000");
-    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+    // Example query
+    let row: (i32,) = sqlx::query_as("SELECT 1")
+        .fetch_one(&pool)
+        .await?;
+
+    println!("Query result: {}", row.0);
+
+    Ok(())
 }
